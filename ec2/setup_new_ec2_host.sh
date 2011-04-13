f0e202d7533726e912efdea832d0c001ed7109da
Sameep Tandon <sameep@berkeley.edu>
2011-04-13 16:22:41 -0700

    updating ec2 host creation script

--- ec2/setup_new_ec2_host.sh
+++ ec2/setup_new_ec2_host.sh
@@ -5,9 +5,7 @@
 
 
 sudo apt-get update
-sudo apt-get install xserver-xorg xserver-xorg-core openbox
-sudo apt-get install g++ gcc libeigen2-dev libgle3 libgle3-dev fortran-compiler libglut3 libglut
-sudo apt-get install subversion git-core
+sudo apt-get install xserver-xorg xserver-xorg-core openbox g++ gcc libeigen2-dev libgle3 libgle3-dev fortran-compiler libglut3 libglut3-dev subversion git-core build-essential libavformat-dev ffmpeg libcv2.1 libcvaux2.1 libhighgui2.1 python-opencv opencv-doc libcv-dev libcvaux-dev libhighgui-dev libboost-dev
 
 #ssh -i sameep-surgical.pem ubuntu@ec2-50-16-35-135.compute-1.amazonaws.com 'cd /home/ubuntu/code/trunk/surgical; svn up --config-option config:tunnels:ssh="ssh -a -i /home/ubuntu/.ssh/id_rsa_st"'
 
